9c8ac822620d298db0621437dc6af2fb78dfd7c9
jacivaldocarvalho <jacivaldocarvalho@gmail.com>
2022-03-03 19:26:43 -0300

    update inicial gerador de citacoes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projeto Gerador de Citações em App React com implementação de pipeline CI/CD.
+## Resumo
+O aplicativo gerador de citações foi desenvolvido para ilustrar a criação de um pipeline de CI/CD para projeto React com CircleCI e Heroku.
+
+
+
---
+++ src/db/quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+  {
+    quote: 'A vida dá lições que só se dão uma vez.',
+    character: 'Winston Churchill',
+  },
+  {
+    quote: 'Quando eu pensar que aprendi a viver, terei aprendido a morrer.',
+    character: 'Leonardo da Vinci',
+  },
+  {
+    quote:
+      "A maioria das pessoas só aprende as lições da vida depois que a mão dura do destino lhe toca no ombro.",
+    character: 'Napoleon Hill',
+  },
+  {
+    quote:
+      "Para mim, é impossível existir sem sonho. A vida na sua totalidade me ensinou como grande lição que é impossível assumi-la sem risco.",
+    character: 'Paulo Freire',
+  },
+  {
+    quote: 'Aprendi que são os pequenos acontecimentos diários que tornam a vida espetacular.',
+    character: 'William Shakespeare',
+  },
+];
+
+export default quotes;
